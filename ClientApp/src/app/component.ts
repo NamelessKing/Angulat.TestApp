18e2825ea8409f85abb274c239ef4c932fd0b652
namelessking <habibur.tanjin@gmail.com>
2019-07-09 12:58:14 +0200

    tests

--- ClientApp/src/app/component.ts
+++ ClientApp/src/app/component.ts
@@ -1,4 +1,5 @@
 
+//09/07/19 12:42
 import { ApplicationRef, Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Model } from "./repository.model";
@@ -8,43 +9,58 @@ import { Product } from "./product.model";
   templateUrl: "template.html"
 })
 export class ProductComponent {
+
   model: Model = new Model();
+
   getProduct(key: number): Product {
     return this.model.getProduct(key);
   }
+
   getProducts(): Product[] {
     return this.model.getProducts();
   }
+
   newProduct: Product = new Product();
+
   get jsonProduct() {
     return JSON.stringify(this.newProduct);
   }
+
   addProduct(p: Product) {
-    console.log("New Product: " + this.jsonProduct);
+    //console.log("New Product: " + this.jsonProduct);
+    this.model.saveProduct(p);
+
   }
+
   getValidationMessages(state: any, thingName?: string) {
     let thing: string = state.path || thingName;
     let messages: string[] = [];
+
     if (state.errors) {
       for (let errorName in state.errors) {
+
         switch (errorName) {
           case "required":
             messages.push(`You must enter a ${thing}`);
             break;
           case "minlength":
             messages.push(`A ${thing} must be at least
-${state.errors['minlength'].requiredLength} characters`);
+                ${state.errors['minlength'].requiredLength} characters`);
             break;
           case "pattern":
             messages.push(`The ${thing} contains illegal
-characters`);
+                characters`);
             break;
         }
+
       }
     }
+
     return messages;
   }
+
   formSubmitted: boolean = false;
+
   submitForm(form: NgForm) {
     this.formSubmitted = true;
     if (form.valid) {
@@ -54,6 +70,7 @@ characters`);
       this.formSubmitted = false;
     }
   }
+
   getFormValidationMessages(form: NgForm): string[] {
     let messages: string[] = [];
     Object.keys(form.controls).forEach(k => {
@@ -62,6 +79,8 @@ characters`);
     });
     return messages;
   }
+
+
 }
 
 
@@ -175,7 +194,7 @@ characters`);
   //  return Math.floor(this.getProduct(index).price);
   //}
 
-}
+//}
 
 
 
