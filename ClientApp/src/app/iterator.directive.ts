a687069c9c235d3690fed0ed7f30aca573b18516
namelessking <habibur.tanjin@gmail.com>
2019-07-10 10:43:21 +0200

    created iterato directive

---
+++ ClientApp/src/app/iterator.directive.ts
@@ -0,0 +1,48 @@
+import { Directive, Input, ViewContainerRef, TemplateRef, SimpleChange } from "@angular/core";
+import { forEach } from "@angular/router/src/utils/collection";
+
+
+@Directive({
+  selector: "[paForOf]"
+})
+export class PaIteratorDirective {
+
+  constructor(private container: ViewContainerRef, private child: TemplateRef<Object>) {
+
+  }
+
+  @Input("paForOf")
+  dataSource: any;
+
+  ngOnInit() {
+    this.container.clear();
+
+    for (let i = 0; i < this.dataSource.length; i++) {
+      this.container.createEmbeddedView
+        (this.child, new PaIteratorContext(this.dataSource[i], i, this.dataSource.length));
+    }
+  }
+}
+
+
+class PaIteratorContext {
+
+  odd: boolean;
+  even: boolean;
+  first: boolean;
+  last: boolean;
+
+
+  constructor(public $implicit: any, public index: number, public total: number) {
+    this.odd = index % 2 == 1;
+    this.even = !this.odd;
+    this.first = index == 0;
+    this.last = index == total - 1;
+
+    //setInterval(() => {
+    //  this.odd = !this.odd;
+    //  this.even = !this.even;
+    //  this.$implicit.price++;
+    //},2000)
+  }
+}
